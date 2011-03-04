e5f0d74f7bb68215692d581c3cf2813f3242c292
Luigi Marini <lmarini@ncsa.illinois.edu>
2011-03-04 16:04:07 +0000

    minor tweaks
    git-svn-id: file:///home/kooper/svn2git/repo/trunk@253 1d209ac1-a921-4c1d-9267-735cbf3ade97

--- resources/duplicates.py
+++ resources/duplicates.py
@@ -1,12 +1,18 @@
 import sys, os
 import itertools
 
-fileSet = []
-for arg in sys.argv:
-    for root, dirs, files in os.walk(arg):
-        for file in files:
-            print file
-            fileSet.append(file)
+def compare(element):
+    pass
 
-for element in itertools.combinations(fileSet, 2):
-    print "Comparing ", element
+if __name__ == '__main__':
+    fileSet = []
+    for arg in sys.argv:
+        for root, dirs, files in os.walk(arg):
+           for file in files:
+                print 'File found: ', file
+                fileSet.append(file)
+
+    for element in itertools.combinations(fileSet, 2):
+        print 'Comparing files ', element
+        compare(element)
+
