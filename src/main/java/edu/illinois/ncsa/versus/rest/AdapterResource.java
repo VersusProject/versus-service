0b9bc585a6d1d3013bda64084b7ff217190ada75
Luigi Marini <lmarini@illinois.edu>
2012-06-22 17:06:20 -0500

    Stared refactoring to jax-rs with resteasy.

---
+++ src/main/java/edu/illinois/ncsa/versus/rest/AdapterResource.java
@@ -0,0 +1,67 @@
+/**
+ * 
+ */
+package edu.illinois.ncsa.versus.rest;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+import javax.servlet.ServletContext;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
+
+import edu.illinois.ncsa.versus.adapter.Adapter;
+import edu.illinois.ncsa.versus.registry.CompareRegistry;
+
+/**
+ * Adapters resource.
+ * 
+ * @author Luigi Marini
+ * 
+ */
+@Path("/adapters")
+public class AdapterResource {
+
+	@GET
+	public String list(@Context ServletContext context) {
+
+		CompareRegistry registry = (CompareRegistry) context
+				.getAttribute(CompareRegistry.class.getName());
+
+		Collection<Adapter> adapters = registry.getAvailableAdapters();
+
+		if (adapters.size() == 0) {
+			return "No adapters";
+		} else {
+			String content = new String("<h3>Versus > Adapters</h3>" + "<ul>");
+			for (Adapter adapter : adapters) {
+				content += "<li><a href='/versus/api/adapters/"
+						+ adapter.getClass().getName() + "'>"
+						+ adapter.getClass().getName() + "</a></li>";
+			}
+			content += "</ul>";
+			return content;
+		}
+	}
+
+	@GET
+	@Consumes("application/json")
+	@Produces("application/json")
+	public List<Adapter> listJSON(@Context ServletContext context) {
+
+		CompareRegistry registry = (CompareRegistry) context
+				.getAttribute(CompareRegistry.class.getName());
+
+		Collection<Adapter> adapters = registry.getAvailableAdapters();
+
+		if (adapters.size() == 0) {
+			return new ArrayList<Adapter>();
+		} else {
+			return new ArrayList<Adapter>(adapters);
+		}
+	}
+}
