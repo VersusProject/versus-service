3e7ab3491bcbd51784743d5ac1170a8bab7ec705
Luigi Marini <lmarini@illinois.edu>
2012-07-12 21:10:12 -0500

    Mongo implementation of comparison processor.

--- src/main/java/edu/illinois/ncsa/versus/store/MongoFileProcessor.java
+++ src/main/java/edu/illinois/ncsa/versus/store/MongoFileProcessor.java
@@ -7,7 +7,6 @@ import java.io.InputStream;
 
 import org.bson.types.ObjectId;
 
-import com.mongodb.Mongo;
 import com.mongodb.gridfs.GridFS;
 import com.mongodb.gridfs.GridFSDBFile;
 import com.mongodb.gridfs.GridFSInputFile;
@@ -18,24 +17,8 @@ import com.mongodb.gridfs.GridFSInputFile;
  * @author Luigi Marini
  * 
  */
-public class MongoFileProcessor implements FileProcessor {
-
-	private static String host = "127.0.0.1";
-	private static String db = "test";
-	private static Mongo mongo = null;
-	private static GridFS gridFS;
-
-	private static Mongo getMongo() throws Exception {
-		if (mongo == null)
-			mongo = new Mongo(host);
-		return mongo;
-	}
-
-	private static GridFS getGridFS() throws Exception {
-		if (gridFS == null)
-			gridFS = new GridFS(getMongo().getDB(db));
-		return gridFS;
-	}
+public class MongoFileProcessor extends MongoConnection implements
+		FileProcessor {
 
 	@Override
 	public String addFile(InputStream inputStream, String filename)
