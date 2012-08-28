24ef4f110863f0fc36425fc7736145321f351582
Antoine Vandecreme <antoine.vandecreme@nist.gov>
2012-08-28 11:25:33 -0400

    Allow to specify measures tests to run

--- versus-service-server/src/main/java/gov/nist/itl/ssd/similarity/test/execution/TestServerResource.java
+++ versus-service-server/src/main/java/gov/nist/itl/ssd/similarity/test/execution/TestServerResource.java
@@ -1,5 +1,8 @@
 package gov.nist.itl.ssd.similarity.test.execution;
 
+import java.util.List;
+
+import org.apache.commons.lang.StringUtils;
 import org.restlet.data.MediaType;
 import org.restlet.data.Reference;
 import org.restlet.representation.Representation;
@@ -11,15 +14,21 @@ import gov.nist.itl.ssd.similarity.test.Tool;
 
 public class TestServerResource extends VersusServerResource {
 
-    private static final Tool testingTool = new Tool();
-
     @Get
     public Representation retrieve() {
-        String file = testingTool.test("EuclideanL2Measure");
+        String command = (String) getRequest().getAttributes().get("command");
         Reference reference = getReference();
-        String refString = reference.toString();
-        String url = refString.substring(0, refString.lastIndexOf(reference.getLastSegment().toString()));
-        redirectTemporary(url + "testsresult/" + file);
+        List<String> segments = reference.getSegments();
+        segments.remove(segments.size() - 1);
+        String file;
+        if (command == null) {
+            file = new Tool().test();
+        } else {
+            file = new Tool().test(command);
+            segments.remove(segments.size() - 1);
+        }
+        String url = "http://" + reference.getAuthority() + '/' + StringUtils.join(segments, '/');
+        redirectTemporary(url + "/testsresult/" + file);
         return new StringRepresentation("Redirecting to html help.", MediaType.TEXT_PLAIN);
     }
 }
