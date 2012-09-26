ff774c2cf8fa79e717799d45031e78509a4be514
Antoine Vandecreme <antoine.vandecreme@nist.gov>
2012-09-26 10:10:17 -0400

    Fix properties loading

--- versus-service-server/src/main/java/edu/illinois/ncsa/versus/restlet/PropertiesUtil.java
+++ versus-service-server/src/main/java/edu/illinois/ncsa/versus/restlet/PropertiesUtil.java
@@ -1,26 +1,44 @@
 /**
- * 
+ *
  */
 package edu.illinois.ncsa.versus.restlet;
 
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.Properties;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  * Properties on disk in versus.properties
- * 
+ *
  * @author Luigi Marini <lmarini@ncsa.illinois.edu>
  */
 public class PropertiesUtil {
 
-	public static Properties load() throws IOException {
-		Properties defaultProps = new Properties();
-		InputStream inputStream = PropertiesUtil.class.getClassLoader().getResourceAsStream("versus.properties"); 
-//		FileInputStream inputStream = new FileInputStream("versus.properties");
-		defaultProps.load(inputStream);
-		inputStream.close();
-		return defaultProps;
-	}
+    public static Properties load() throws IOException {
+        Properties defaultProps = new Properties();
+        File props = new File("versus.properties");
+
+        InputStream inputStream;
+        if (props.exists()) {
+            inputStream = new FileInputStream(props);
+            Logger.getLogger(PropertiesUtil.class.getName()).log(Level.INFO, 
+                    "Loading properties from versus.properties file");
+        } else {
+            inputStream = PropertiesUtil.class.getClassLoader()
+                    .getResourceAsStream("versus.properties");
+            Logger.getLogger(PropertiesUtil.class.getName()).log(Level.INFO, 
+                    "Loading properties from versus.properties jar resource");
+        }
+        try {
+            defaultProps.load(inputStream);
+        } finally {
+            inputStream.close();
+        }
+
+        return defaultProps;
+    }
 }
