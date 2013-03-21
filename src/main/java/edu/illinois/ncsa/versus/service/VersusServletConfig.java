c9b8ba395b31a82b05e54452bba82a11e1b9ff1b
smpadhy <spadhy@illinois.edu>
2013-03-21 09:48:22 -0500

    Servlet configuation for indexing

--- src/main/java/edu/illinois/ncsa/versus/service/VersusServletConfig.java
+++ src/main/java/edu/illinois/ncsa/versus/service/VersusServletConfig.java
@@ -9,10 +9,23 @@ import javax.servlet.ServletContext;
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
 
+//import main.java.edu.illinois.ncsa.versus.census.CensusIndexer;
+//import main.java.edu.illinois.ncsa.versus.census.WordspottingExtractor;
+import edu.illinois.ncsa.versus.adapter.impl.BufferedImageAdapter;
+
+
+
+import edu.illinois.ncsa.versus.descriptor.Descriptor;
 import edu.illinois.ncsa.versus.engine.impl.ExecutionEngine;
+import edu.illinois.ncsa.versus.engine.impl.IndexingEngine;
+import edu.illinois.ncsa.versus.extract.impl.RGBHistogramExtractor;
 import edu.illinois.ncsa.versus.registry.CompareRegistry;
+import edu.illinois.ncsa.versus.rest.CensusIndexer;
 import edu.illinois.ncsa.versus.rest.HashIdSlave;
+import edu.illinois.ncsa.versus.rest.Index;
+import edu.illinois.ncsa.versus.rest.LinearIndexer;
 import edu.illinois.ncsa.versus.rest.SlavesList;
+import edu.illinois.ncsa.versus.rest.WordspottingExtractor;
 
 /**
  * Load Versus registry in servlet context.
@@ -36,12 +49,29 @@ public class VersusServletConfig implements ServletContextListener {
 	public void contextInitialized(ServletContextEvent event) {
 		CompareRegistry registry = new CompareRegistry();
 		ExecutionEngine engine = new ExecutionEngine();
+		
+		
+		//LinearIndexer indexer= new LinearIndexer();
+		
+		//IndexingEngine inengine=new IndexingEngine(indexer,2);
+		
+		IndexingEngine inengine=new IndexingEngine(2);
+		//IndexingEngine inengine=new IndexingEngine(adapter,extractor,indexer,2);    // adding for Indexing
+		
 		SlavesList slaveList=new SlavesList();  //added for master-slave config
 		HashIdSlave compList=new HashIdSlave(); // added for master-slave config
+		
+		//Index indexHash=new Index();
+		
 		ServletContext context = event.getServletContext();
 		context.setAttribute(CompareRegistry.class.getName(), registry);
 		context.setAttribute(ExecutionEngine.class.getName(), engine);
+		
+		context.setAttribute(IndexingEngine.class.getName(),inengine);
+		//context.setAttribute(CensusIndexer.class.getName(),indexer);
+		
 		context.setAttribute(SlavesList.class.getName(),slaveList); //added for master-slave config
 		context.setAttribute(HashIdSlave.class.getName(), compList);
+		//context.setAttribute(Index.class.getName(), indexHash);
 	}
 }
