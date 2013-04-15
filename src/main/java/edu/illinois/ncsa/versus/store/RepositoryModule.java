a1734b281d887225c7242db7bf8c5ee23bd18a25
smpadhy <spadhy@illinois.edu>
2013-04-15 14:06:53 -0500

    modified

--- src/main/java/edu/illinois/ncsa/versus/store/RepositoryModule.java
+++ src/main/java/edu/illinois/ncsa/versus/store/RepositoryModule.java
@@ -6,7 +6,9 @@ import java.util.Properties;
 import com.google.inject.AbstractModule;
 import com.google.inject.Singleton;
 
+import edu.illinois.ncsa.versus.rest.LinearIndexer;
 import edu.illinois.ncsa.versus.restlet.PropertiesUtil;
+import edu.illinois.ncsa.versus.search.Indexer;
 
 /**
  * Guice wiring for repository.
@@ -23,15 +25,15 @@ public class RepositoryModule extends AbstractModule {
 			bind(ComparisonService.class).to(ComparisonServiceImpl.class).in(
 					Singleton.class);
 
-			bind(IndexService.class).to(IndexServiceImpl.class).in(Singleton.class);
 			Properties properties = PropertiesUtil.load();
-			
+
 			// comparison results & Index
 			String repository = properties.getProperty("repository", "mem");
+
 			if ("mem".equals(repository)) {
 				bind(ComparisonProcessor.class).to(
 						InMemoryComparisonProcessor.class).in(Singleton.class);
-				bind(IndexProcessor.class).to(InMemoryIndex.class).in(Singleton.class);
+
 			} else if ("mysql".equals(repository)) {
 				bind(ComparisonProcessor.class).to(
 						JDBCComparisonProcessor.class).in(Singleton.class);
@@ -41,6 +43,7 @@ public class RepositoryModule extends AbstractModule {
 			}
 			// files
 			String files = properties.getProperty("files", "disk");
+
 			if ("disk".equals(files)) {
 				bind(FileProcessor.class).to(DiskFileProcessor.class).in(
 						Singleton.class);
@@ -48,12 +51,46 @@ public class RepositoryModule extends AbstractModule {
 				bind(FileProcessor.class).to(MongoFileProcessor.class).in(
 						Singleton.class);
 			}
+
+			// index
+			bind(IndexService.class).to(IndexServiceImpl.class).in(
+					Singleton.class);
+
+			String index = properties.getProperty("index", "memIndex");
+
+			if ("memIndex".equals(index)) {
+				bind(IndexProcessor.class).to(InMemoryIndex.class).in(
+						Singleton.class);
+			} else if ("diskIndex".equals(index)) {
+				bind(IndexProcessor.class).to(DiskIndexProcessor.class).in(
+						Singleton.class);
+			}
+
+			// indexer
+			bind(IndexerService.class).to(IndexerServiceImpl.class).in(
+					Singleton.class); // how to bind it to census Indexer also
+			// bind(IndexerProcessor.class).to(LinearIndexer.class).in(Singleton.class);
+
+			// String indexer=properties.getProperty("indexer","memLinear");
+
+			/*
+			 * if("memLinear".equals(indexer)){
+			 * bind(IndexerProcessor.class).to(InMemoryLinearIndexer
+			 * .class).in(Singleton.class);
+			 * 
+			 * }
+			 */
+
 			// distribution
-			bind(DistributionService.class).to(DistributionServiceImpl.class).in(Singleton.class);
-			//decision support
-			bind(DecisionSupportService.class).to(DecisionSupportServiceImpl.class).in(Singleton.class);
-			//mlds
-			bind(MultiLabelDecisionSupportService.class).to(MultiLabelDecisionSupportServiceImpl.class).in(Singleton.class);
+			bind(DistributionService.class).to(DistributionServiceImpl.class)
+					.in(Singleton.class);
+			// decision support
+			bind(DecisionSupportService.class).to(
+					DecisionSupportServiceImpl.class).in(Singleton.class);
+			// mlds
+			bind(MultiLabelDecisionSupportService.class).to(
+					MultiLabelDecisionSupportServiceImpl.class).in(
+					Singleton.class);
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
